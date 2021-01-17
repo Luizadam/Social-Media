57fef1232ad2bed4a29064d1e80be7d14fa96a8a
Luizadam <luizadaam@gmail.com>
2021-01-17 23:37:57 +0700

    hmm

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.use(express.static(__dirname + '/dist/sosmed'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/sosmed/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
